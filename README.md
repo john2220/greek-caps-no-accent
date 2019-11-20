17dc0028352249a7e8839ff3c55021ea59de1f87
John Raptis <johnjohn_raptis@yahoo.gr>
2019-11-20 13:27:21 +0200

    changed class name

---
+++ README.md
@@ -0,0 +1,4 @@
+### Greek Caps No Accents
+
+This is a package to help you remove the accent from greek letters when capitalized.
+Simply add the "caps" class and you are all set.
--- app.js
+++ app.js
@@ -1,42 +1,40 @@
-window.addEventListener('load', function() {
-   const stress = document.querySelectorAll('.word');
+window.addEventListener("load", function() {
+   const stress = document.querySelectorAll(".caps");
 
-   stress.forEach((eachWord) => {
-         let word = eachWord.textContent;
+   stress.forEach(eachWord => {
+      let word = eachWord.textContent;
 
-         function convertTones(word) {
-            let newLetter = [];  
+      function convertTones(word) {
+         let newLetter = [];
 
-            for (var i = 0; i < word.length; i++) {
-               if(word.charAt(i) === 'ά') {
-                  let greek_a = word.charAt(i).value = 'α';
-                  newLetter.push(greek_a);
-               } else if(word.charAt(i) === 'έ') {
-                  let greek_e = word.charAt(i).value = 'ε';
-                  newLetter.push(greek_e);
-               }  else if(word.charAt(i) === 'ή') {
-                  let greek_e = word.charAt(i).value = 'η';
-                  newLetter.push(greek_e);
-               } else if(word.charAt(i) === 'ί') {
-                  let greek_e = word.charAt(i).value = 'ι';
-                  newLetter.push(greek_e);
-               } else if(word.charAt(i) === 'ό') {
-                  let greek_e = word.charAt(i).value = 'ο';
-                  newLetter.push(greek_e);
-               } else if(word.charAt(i) === 'ύ') {
-                  let greek_e = word.charAt(i).value = 'υ';
-                  newLetter.push(greek_e);
-               }  else if(word.charAt(i) === 'ώ') {
-                  let greek_e = word.charAt(i).value = 'ω';
-                  newLetter.push(greek_e);
-               } else {
-                  newLetter.push(word.charAt(i));
-               }
+         for (var i = 0; i < word.length; i++) {
+            if (word.charAt(i) === "ά") {
+               let greek_a = (word.charAt(i).value = "α");
+               newLetter.push(greek_a);
+            } else if (word.charAt(i) === "έ") {
+               let greek_e = (word.charAt(i).value = "ε");
+               newLetter.push(greek_e);
+            } else if (word.charAt(i) === "ή") {
+               let greek_e = (word.charAt(i).value = "η");
+               newLetter.push(greek_e);
+            } else if (word.charAt(i) === "ί") {
+               let greek_e = (word.charAt(i).value = "ι");
+               newLetter.push(greek_e);
+            } else if (word.charAt(i) === "ό") {
+               let greek_e = (word.charAt(i).value = "ο");
+               newLetter.push(greek_e);
+            } else if (word.charAt(i) === "ύ") {
+               let greek_e = (word.charAt(i).value = "υ");
+               newLetter.push(greek_e);
+            } else if (word.charAt(i) === "ώ") {
+               let greek_e = (word.charAt(i).value = "ω");
+               newLetter.push(greek_e);
+            } else {
+               newLetter.push(word.charAt(i));
             }
-            eachWord.innerHTML = newLetter.join('').toUpperCase();
          }
-         convertTones(word); 
-
+         eachWord.innerHTML = newLetter.join("").toUpperCase();
+      }
+      convertTones(word);
    });
 });
-
